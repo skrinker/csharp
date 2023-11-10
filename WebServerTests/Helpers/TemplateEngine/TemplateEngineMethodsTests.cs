66b38eb8bf0baef7156b67ff8758efe48d903d4d
Bulat Biktagirov <b.bktrg@gmail.com>
2023-11-10 23:21:15 +0300

    template enigne method #1

---
+++ WebServerTests/Helpers/TemplateEngine/TemplateEngineMethodsTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebServer.Helpers.TemplateEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer.Helpers.TemplateEngine.Tests
+{
+    [TestClass()]
+    public class TemplateEngineMethodsTests
+    {
+        [TestMethod()]
+        public void SubstituteStringTest()
+        {
+            var methods = new TemplateEngineMethods();
+            string expected = "Hello World";
+
+            Assert.AreEqual(expected, methods.SubstituteString("Hello @{var}", "World"));
+        }
+    }
+}
---
+++ hw_2/Helpers/TemplateEngine/TemplateEngineMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebServer.Helpers.TemplateEngine
+{
+    public class TemplateEngineMethods
+    {
+
+        public string SubstituteString(string input, string replacement) 
+        {
+            string pattern = @"@\{([a-zA-Z]+)\}";
+            return Regex.Replace(input, pattern, replacement);
+        }
+    }
+}
